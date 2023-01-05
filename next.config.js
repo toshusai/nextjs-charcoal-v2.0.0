721c5553ed2cc7219a476f6eb0b0882a2c171ef1
toshusai <toshusai.net@gmail.com>
2023-01-05 17:54:57 +0900

    feat: add charcoal

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    esmExternals: true
+  },
+  compiler: {
+    styledComponents: true,
+  },
+  /** @param {import('webpack').Configuration} [config] */
+  webpack(config) {
+    return config
+  },
 }
 
 module.exports = nextConfig
